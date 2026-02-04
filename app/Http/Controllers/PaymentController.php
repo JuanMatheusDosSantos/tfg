293da736cf8ba22211094d14a6f22560dbfe68fc
JuanMaheusDosSantos <daw04.2025.jesuitas@gmail.com>
2026-02-04 21:08:02 +0100

    21:00 04/02/2026

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $payments=Payment::all();
+            return request()->json($payments);
+        }catch (\Exception $e){
+            return  response()->json(["ha habido un fallo a la hora de mostrar los pagos"],500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
