c992abd81b1d670c65c242a457bbe4c56ab57e15
JuanMaheusDosSantos <daw04.2025.jesuitas@gmail.com>
2026-04-27 16:04:59 +0200

    seeder de joana

--- Mundo_Marino_back/database/seeders/UserSeeder.php
+++ Mundo_Marino_back/database/seeders/UserSeeder.php
@@ -14,15 +14,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-//        DB::table('users')->insert([
-//           "id"=>1,
-//           "name"=>"admin",
-//            "email" => "prueba@gmail.com",
-//            "password" => bcrypt("12345678"),
-//            "role"=>"admin",
-//            "created_at"=>now(),
-//            "updated_at"=>now()
-//        ]);
         User::factory()->create([
             "name"=>"admin",
             "email"=>"daw04.2025.jesuitas@gmail.com",
@@ -46,6 +37,13 @@ class UserSeeder extends Seeder
             "role" => "restaurant",
             "park_id"=>1,
             "phone" => 555666777
+        ]);
+        User::factory()->create([
+            "name"=>"Joana",
+            "email"=>"daw06.2025@gmail.com",
+            "password"=>bcrypt("12345678"),
+            "role"=>"admin",
+            "phone"=>123456789
         ]);
         User::factory()->count(10)->create();
     }
