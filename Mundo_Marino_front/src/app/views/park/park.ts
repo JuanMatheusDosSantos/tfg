aefa0216a0a95e57eeba0e91c97cd4c14c1e780f
JuanMaheusDosSantos <daw04.2025.jesuitas@gmail.com>
2026-04-30 20:55:31 +0200

    cambios en atracciones

--- Mundo_Marino_front/src/app/views/park/park.ts
+++ Mundo_Marino_front/src/app/views/park/park.ts
@@ -26,6 +26,7 @@ export class Park {
   ngOnInit() {
     this.attractionService.fetchAttractions().subscribe({
       next: (data) => {
+        console.log(data)
         this.atracciones.set(data.filter(a => a.status !== 'permanently_closed'&&a.park?.id===1));
         this.cargando.set(false);
       },
