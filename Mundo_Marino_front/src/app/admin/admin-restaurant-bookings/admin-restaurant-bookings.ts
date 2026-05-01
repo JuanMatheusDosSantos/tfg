a9d0501e012d4e4fa29f950645d5316f24e0c0b8
JuanMaheusDosSantos <daw04.2025.jesuitas@gmail.com>
2026-05-01 14:10:29 +0200

    cambios en admin park

--- Mundo_Marino_front/src/app/admin/admin-restaurant-bookings/admin-restaurant-bookings.ts
+++ Mundo_Marino_front/src/app/admin/admin-restaurant-bookings/admin-restaurant-bookings.ts
@@ -47,7 +47,6 @@ export class AdminRestaurantBookings implements OnInit {
           this.reservas.set(data);
         } else {
           console.log(data)
-          console.log(this.auth.currentUser())
           this.reservas.set(data.filter(r => r.restaurant?.id === this.auth.currentUser()?.restaurant_id));
         }
         this.cargando.set(false);
--- Mundo_Marino_front/src/app/auth/auth.model.ts
+++ Mundo_Marino_front/src/app/auth/auth.model.ts
@@ -2,6 +2,7 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
 import { AuthService} from './auth';
 import {Park} from '../models/park';
+import {Restaurant} from '../models/restaurant';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
@@ -25,5 +26,6 @@ export interface User {
   email: string;
   role:number
   park:Park
+  restaurant:Restaurant
   birthdate: Date;
 }
