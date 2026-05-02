e8150fb934871e698714c30f02dfb30910842dce
JuanMaheusDosSantos <daw04.2025.jesuitas@gmail.com>
2026-05-03 00:08:34 +0200

    cambios en el login para movil

--- Mundo_Marino_back/database/migrations/2026_01_14_192625_attraction_reservations.php
+++ Mundo_Marino_back/database/migrations/2026_01_14_192625_attraction_reservations.php
@@ -11,25 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('attraction_reservations', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId("user_id")
-                ->references("id")
-                ->on("users")
-                ->cascadeOnDelete();
-            $table->foreignId("park_id")
-                ->references("id")
-                ->on("parks")
-                ->cascadeOnDelete();
-            $table->foreignId("attraction_id")
-                ->references("id")
-                ->on("attractions")
-                ->cascadeOnDelete();
-            $table->date("reservation_date");
-            $table->date("time_reservation");
-            $table->timestamps();
-            $table->unique(["user_id","attraction_id","park_id","reservation_date"],"ui_ai_pi_rd");
-        });
+//        Schema::create('attraction_reservations', function (Blueprint $table) {
+//            $table->id();
+//            $table->foreignId("user_id")
+//                ->references("id")
+//                ->on("users")
+//                ->cascadeOnDelete();
+//            $table->foreignId("park_id")
+//                ->references("id")
+//                ->on("parks")
+//                ->cascadeOnDelete();
+//            $table->foreignId("attraction_id")
+//                ->references("id")
+//                ->on("attractions")
+//                ->cascadeOnDelete();
+//            $table->date("reservation_date");
+//            $table->date("time_reservation");
+//            $table->timestamps();
+//            $table->unique(["user_id","attraction_id","park_id","reservation_date"],"ui_ai_pi_rd");
+//        });
     }
 
     /**
--- Mundo_Marino_back/database/migrations/2026_04_05_192732_park_reservations.php
+++ Mundo_Marino_back/database/migrations/2026_04_05_192732_park_reservations.php
@@ -35,7 +35,7 @@ return new class extends Migration
             $table->decimal("child_price_total", 10, 2);
             $table->decimal("applied_tax", 5, 2);
 
-            $table->foreignId("park_reservation_type_id")->constrained("park_reservation_types"); // ← aquí
+            $table->foreignId("park_reservation_type_id")->constrained("park_reservation_types");
 
 
             $table->unique(["user_id","park_id","reservation_date"],"ui_pi_rd");
