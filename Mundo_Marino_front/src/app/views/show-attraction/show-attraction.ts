8b22b48bb21a62bb79776a6f11bddd3e39ed2969
JuanMatheusDosSantos <daw04.2025.jesuitas@gmail.com>
2026-05-04 09:40:11 +0200

    modificiacion de atracciones

--- Mundo_Marino_front/src/app/views/show-attraction/show-attraction.ts
+++ Mundo_Marino_front/src/app/views/show-attraction/show-attraction.ts
@@ -15,6 +15,7 @@ export class ShowAttraction {
   atraccion = signal<Attraction | null>(null);
   loading = signal(true);
   error = signal<string | null>(null);
+  imgUrl=`${environment.imgUrl}/storage/attractions/`
 
   readonly DEFAULT_IMAGE = 'https://lh3.googleusercontent.com/aida-public/AB6AXuCFsEt5IDp4eM3zxQA_qXlmCTKvlR_kWL1l9nQ2uAotEcuvEHEggiUvGBRn8Qwx3jKLnhW2Frj7gBCi8egjueurnHnF5NkqrZJVILn4VPbo2afG-zyvZIfgsBrnRoe-MkMQjdJc5TdAsseFh8rB6HqJRlcWdDoXQTC0wFvNMSPGk-PbMcW7orrjtyDQEJqvTiaUzLAAZMGQ-4ldr4OtJZ1o3DoKPpGWdAt5NNDOocklyDyvny298A7zwtA0g4mIhwnjsWyl__BA4arG';
 
@@ -57,18 +58,18 @@ export class ShowAttraction {
     return map[this.atraccion()?.type ?? ''] ?? '';
   }
 
-  // get imagen(): string {
-  //   const map: Record<string, string> = {
-  //     suave:    'https://lh3.googleusercontent.com/aida-public/AB6AXuCFsEt5IDp4eM3zxQA_qXlmCTKvlR_kWL1l9nQ2uAotEcuvEHEggiUvGBRn8Qwx3jKLnhW2Frj7gBCi8egjueurnHnF5NkqrZJVILn4VPbo2afG-zyvZIfgsBrnRoe-MkMQjdJc5TdAsseFh8rB6HqJRlcWdDoXQTC0wFvNMSPGk-PbMcW7orrjtyDQEJqvTiaUzLAAZMGQ-4ldr4OtJZ1o3DoKPpGWdAt5NNDOocklyDyvny298A7zwtA0g4mIhwnjsWyl__BA4arG',
-  //     moderado: 'https://lh3.googleusercontent.com/aida-public/AB6AXuAEcO_iRCG3VCuDFpcHY9phHqsZRk6ZunwMw_wee_hP7K5GCkBgViTVXMxE8mxuun1b8ZPKBdTNno8XZFyD2vQzW4aJ0dA7G7iH4nuUSLLTczHjP6qs0i1oLZ5HrL2qQx_mh62CXRMPgTQgfC2QYVk-tv6EdV0rbmeYt19mTH1gwgge7YhUNxjxVs5R1NWP_nXoYIlfAoGmQb2I0WRWr_GXDyyrAKb2ge0giJnhqyJDIfu8HFE7HNLgPjGNMEURgwHuqV3VCBAzFYsL',
-  //     intenso:  'https://lh3.googleusercontent.com/aida-public/AB6AXuA-MT7e-6fccK5RDO2FAOUbcpJT5qiBqtwY7znPpJTBQP-eXeiTwtB0HNoR9_p_fZgWfm1-D0OIiPt6VAIzgaxE5pCzpzI9PedktsydRmPo8tfOjV_CFWcqdtcL3UtjGauo15WYAT0SiZXvar3hacQGQZgNO8CpZ5fKHwv0Upn3GEih9r8RrLJlPw0IvQeG5NmIBHkpFvuy98b_Vav2m29pMzmLgVcHbd974OnLOIumFDEXhey-kxYgXk0xQXpffjvFbwBVdks7SUdR',
-  //   };
-  //   return map[this.atraccion()?.type ?? ''] ?? map['suave'];
-  // }
+
 
   get imagen(): string {
-    return this.atraccion()?.image || this.DEFAULT_IMAGE;
-  }
+    if (this.atraccion()?.image?.startsWith('http')) {
+      return <string>this.atraccion()?.image;
+    }
+    else if(this.atraccion()?.image){
+      return this.imgUrl+this.atraccion()?.image
+    }
+    else{
+      return this.DEFAULT_IMAGE;
+    }  }
 
   volver() {
     this.router.navigate(['/attraction']);
